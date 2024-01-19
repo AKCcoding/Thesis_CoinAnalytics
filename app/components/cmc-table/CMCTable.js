281676e06f6722fbdb2b88c2e3058c3626aa0863
AKCcoding <alraeatwork@gmail.com>
2024-01-19 20:36:33 +0800

    Save

--- app/components/cmc-table/CMCTable.js
+++ app/components/cmc-table/CMCTable.js
@@ -1,8 +1,32 @@
 import React, {useState, useContext, useEffect, useCallback} from 'react'
 import btc from "../../assets/png/btc.png"
-import {CoinMarketContext} from "../../context/CoinMarketContext"
+import {CoinMarketContext} from "../../context/context"
 
 const CMCTable = () => {
+
+  let { getTopTenCoins } = useContext(CoinMarketContext)
+  let [coinData, setCoinData] = useState(null)
+
+  useEffect(() => {
+    setData()
+  }, [])
+
+  const setData = useCallback(async () => {
+    try {
+      let apiResponse = await getTopTenCoins()
+      let filteredResponse = []
+
+      for (let i = 0; i < apiResponse.length; i++) {
+        const element = apiResponse[i]
+        if (element.cmc_rank <= 10) filteredResponse.push(element)
+      }
+
+      setCoinData(filteredResponse)
+    } catch (e) {
+      console.log(e.message)
+    }
+  }, [getTopTenCoins])
+
   return (
     <div>CmcTable</div>
   )
---
+++ context/context.js
@@ -0,0 +1,25 @@
+import {createContext, useState , useEffect} from 'react'
+
+export const CoinMarketContext = createContext()
+
+export const CoinMarketProvider = ({children}) => {
+
+    const getTopTenCoins = async () => {
+        try {
+          const res = await fetch('/api/getTopTen')
+          const data = await res.json()
+          return data.data.data
+        } catch (e) {
+          console.log(e.message)
+        }
+      }
+
+    return (
+        <CoinMarketContext.Provider
+        value={{
+            getTopTenCoins
+        }}
+        >{children}
+        </CoinMarketContext.Provider>
+    )
+}
